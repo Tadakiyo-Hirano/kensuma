0b278700baaa6a8d63f270a26791fa1b3b8a6d58
HARAYUKA <tukituki442@icloud.com>
2022-08-04 22:59:19 +0900

    モデルspec記述中

--- spec/factories/special_vehicles.rb
+++ spec/factories/special_vehicles.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :special_vehicle do
-    
+    uuid { SecureRandom.uuid }
+    sequence(:name) { |n| "name#{n}" }
+    sequence(:maker) { |n| "maker#{n}" }
+    check_exp_date_year { '2022-01-30' }
+    check_exp_date_month { '2022-01-30' }
+    check_exp_date_specific { '2022-01-30' }
+    check_exp_date_machine { '2022-01-30' }
+    check_exp_date_car { '2022-01-30' }
+    business
   end
 end
