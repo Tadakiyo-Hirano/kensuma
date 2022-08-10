4d55dc03634daceb933135a8ea1f955de5918051
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-08-10 14:50:51 +0900

    関連付け設定

--- app/models/field_car.rb
+++ app/models/field_car.rb
@@ -1,2 +1,12 @@
 class FieldCar < ApplicationRecord
+  belongs_to :field_carable, polymorphic: true
+
+  before_create -> { self.uuid = SecureRandom.uuid }
+
+  validates :car_name, presence: true
+  validates :content, presence: true
+
+  def to_param
+    uuid
+  end
 end
