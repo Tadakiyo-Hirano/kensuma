e1639855b8d96832fdada76a4b2df0e2239f3373
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-08-17 16:56:23 +0900

    Rspec追加

--- spec/factories/field_cars.rb
+++ spec/factories/field_cars.rb
@@ -5,6 +5,13 @@ FactoryBot.define do
     uuid { SecureRandom.uuid }
     sequence(:car_name) { |n| "car_name#{n}" }
     sequence(:content) { |n| { "id": n + 1 } }
+    sequence(:driver_name) { |n| "driver_name#{n}" }
+    usage_period_start { '2022-01-01' }
+    usage_period_end { '2022-01-01' }
+    sequence(:starting_point) { |n| "starting_point#{n}" }
+    sequence(:waypoint_first) { |n| "waypoint_first#{n}" }
+    sequence(:waypoint_second) { |n| "waypoint_second#{n}" }
+    sequence(:arrival_point) { |n| "arrival_point#{n}" }
   end
 
   factory :request_order_field_car, class: 'FieldCar' do
@@ -13,5 +20,12 @@ FactoryBot.define do
     uuid { SecureRandom.uuid }
     sequence(:car_name) { |n| "car_name#{n}" }
     sequence(:content) { |n| { "id": n + 1 } }
+    sequence(:driver_name) { |n| "driver_name#{n}" }
+    usage_period_start { '2022-01-01' }
+    usage_period_end { '2022-01-01' }
+    sequence(:starting_point) { |n| "starting_point#{n}" }
+    sequence(:waypoint_first) { |n| "waypoint_first#{n}" }
+    sequence(:waypoint_second) { |n| "waypoint_second#{n}" }
+    sequence(:arrival_point) { |n| "arrival_point#{n}" }
   end
 end
--- spec/models/field_car_spec.rb
+++ spec/models/field_car_spec.rb
@@ -30,6 +30,78 @@ RSpec.describe FieldCar, type: :model do
         end
       end
     end
+
+    describe '#starting_point' do
+      context '文字数が40文字の場合' do
+        before(:each) { subject.starting_point = 'a' * 40 }
+
+        it 'バリデーションに通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '文字数が41文字の場合' do
+        before(:each) { subject.starting_point = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#waypoint_first' do
+      context '文字数が40文字の場合' do
+        before(:each) { subject.waypoint_first = 'a' * 40 }
+
+        it 'バリデーションに通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '文字数が41文字の場合' do
+        before(:each) { subject.waypoint_first = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#waypoint_second' do
+      context '文字数が40文字の場合' do
+        before(:each) { subject.waypoint_second = 'a' * 40 }
+
+        it 'バリデーションに通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '文字数が41文字の場合' do
+        before(:each) { subject.waypoint_second = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#arrival_point' do
+      context '文字数が40文字の場合' do
+        before(:each) { subject.arrival_point = 'a' * 40 }
+
+        it 'バリデーションに通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '文字数が41文字の場合' do
+        before(:each) { subject.arrival_point = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
   end
 
   describe 'request_order側バリデーションについて' do
@@ -52,6 +124,78 @@ RSpec.describe FieldCar, type: :model do
     describe '#content' do
       context '存在しない場合' do
         before(:each) { subject.content = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#starting_point' do
+      context '文字数が40文字の場合' do
+        before(:each) { subject.starting_point = 'a' * 40 }
+
+        it 'バリデーションに通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '文字数が41文字の場合' do
+        before(:each) { subject.starting_point = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#waypoint_first' do
+      context '文字数が40文字の場合' do
+        before(:each) { subject.waypoint_first = 'a' * 40 }
+
+        it 'バリデーションに通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '文字数が41文字の場合' do
+        before(:each) { subject.waypoint_first = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#waypoint_second' do
+      context '文字数が40文字の場合' do
+        before(:each) { subject.waypoint_second = 'a' * 40 }
+
+        it 'バリデーションに通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '文字数が41文字の場合' do
+        before(:each) { subject.waypoint_second = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#arrival_point' do
+      context '文字数が40文字の場合' do
+        before(:each) { subject.arrival_point = 'a' * 40 }
+
+        it 'バリデーションに通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '文字数が41文字の場合' do
+        before(:each) { subject.arrival_point = 'a' * 41 }
 
         it 'バリデーションに落ちること' do
           expect(subject).to be_invalid
