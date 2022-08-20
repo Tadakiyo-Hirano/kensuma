f3f3909252fb6ed4fb4033d5443ebadf51391cfc
ikd-gifu <oki19ki@gmail.com>
2022-08-20 15:10:44 +0900

    Define-enum-&-fix-seed-file

--- app/models/business.rb
+++ app/models/business.rb
@@ -11,11 +11,11 @@ class Business < ApplicationRecord
   accepts_nested_attributes_for :business_occupations, allow_destroy: true
 
   enum business_type: { corporation: 0, freelance: 1, Individual_five_over: 2, Individual_five_less: 3 }
-  enum business_health_insurance_status: { join: 0, not_join: 1, not_coverd: 2 }               # 健康保険(加入状況)
-  enum business_welfare_pension_insurance_join_status: { join: 0, not_join: 1, not_coverd: 2 } # 厚生年金保険(加入状況)
-  enum business_pension_insurance_join_status: { welfare: 0, national: 1, recipient: 2 }       # 年金保険(加入状況)
-  enum business_employment_insurance_join_status: { join: 0, not_join: 1, not_coverd: 2 }      # 雇用保険(加入状況)
-  enum business_retirement_benefit_mutual_aid_status: { available: 0, not_available: 1 }       # 退職金共済制度(加入状況)
+  enum business_health_insurance_status: { join: 0, not_join: 1, not_coverd: 2 }, _prefix: true               # 健康保険(加入状況)
+  enum business_welfare_pension_insurance_join_status: { join: 0, not_join: 1, not_coverd: 2 }, _prefix: true # 厚生年金保険(加入状況)
+  enum business_pension_insurance_join_status: { welfare: 0, national: 1, recipient: 2 }                      # 年金保険(加入状況)
+  enum business_employment_insurance_join_status: { join: 0, not_join: 1, not_coverd: 2 }, _prefix: true      # 雇用保険(加入状況)
+  enum business_retirement_benefit_mutual_aid_status: { available: 0, not_available: 1 }                      # 退職金共済制度(加入状況)
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :uuid, presence: true
