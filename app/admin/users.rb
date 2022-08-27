857ce9b3e8a9036e70a0e5e5999536c414964be4
HARAYUKA <tukituki442@icloud.com>
2022-08-28 00:25:52 +0900

    システム管理画面のユーザー作成を元請ユーザーのみに変更等

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,31 +5,31 @@ ActiveAdmin.register User do
   # Uncomment all parameters which should be permitted for assignment
   #
 
-  permit_params :email, :password, :password_confirmation, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :name, :age, :gender, :role
+  permit_params :email, :password, :password_confirmation, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :name, :role
+
+  controller do
+    def scoped_collection
+      end_of_association_chain.where(role: "admin")
+    end
+  end
 
   index do
     column :name
     column :email
-    column :age
-    column :gender
-    column :role
+    column :role do |user|
+      user.role_i18n
+    end
 
     # 閲覧編集削除などのリンク表示
     actions
   end
 
   filter :email
-  filter :age
-  filter :gender
-  filter :role
 
   show do
     attributes_table do
       row :name
       row :email
-      row :age
-      row :gender
-      row :role
     end
   end
 
@@ -37,11 +37,9 @@ ActiveAdmin.register User do
     f.inputs do
       f.input :name
       f.input :email
-      f.input :age
-      f.input :gender
-      f.input :role
       f.input :password
       f.input :password_confirmation
+      f.hidden_field :role, :value => "admin"
     end
     f.actions
   end
