e3a70d6dda0ae17db484bfd83ba0ef22c8773752
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-09-14 20:33:10 +0900

    syntax修正

--- spec/models/field_special_vehicle_spec.rb
+++ spec/models/field_special_vehicle_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe FieldSpecialVehicle, type: :model do
     it 'バリデーションが通ること' do
       expect(subject).to be_valid
     end
-    
+
     describe '#content' do
       context '存在しない場合' do
         before(:each) { subject.content = nil }
@@ -24,6 +24,60 @@ RSpec.describe FieldSpecialVehicle, type: :model do
     describe '#vehicle_name' do
       context '存在しない場合' do
         before(:each) { subject.vehicle_name = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#use_place' do
+      context '100文字の場合' do
+        before(:each) { subject.use_place = 'a' * 100 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.use_place = 'a' * 101 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#contact_prevention' do
+      context '40文字の場合' do
+        before(:each) { subject.contact_prevention = 'a' * 40 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '41文字の場合' do
+        before(:each) { subject.contact_prevention = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#precautions' do
+      context '500文字の場合' do
+        before(:each) { subject.precautions = 'a' * 500 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '501文字の場合' do
+        before(:each) { subject.precautions = 'a' * 501 }
 
         it 'バリデーションに落ちること' do
           expect(subject).to be_invalid
@@ -52,6 +106,60 @@ RSpec.describe FieldSpecialVehicle, type: :model do
     describe '#vehicle_name' do
       context '存在しない場合' do
         before(:each) { subject.vehicle_name = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#use_place' do
+      context '100文字の場合' do
+        before(:each) { subject.use_place = 'a' * 100 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.use_place = 'a' * 101 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#contact_prevention' do
+      context '40文字の場合' do
+        before(:each) { subject.contact_prevention = 'a' * 40 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '41文字の場合' do
+        before(:each) { subject.contact_prevention = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#precautions' do
+      context '500文字の場合' do
+        before(:each) { subject.precautions = 'a' * 500 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '501文字の場合' do
+        before(:each) { subject.precautions = 'a' * 501 }
 
         it 'バリデーションに落ちること' do
           expect(subject).to be_invalid
