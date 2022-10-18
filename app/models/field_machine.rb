122a90286f76e2d56c7afe7cb4d6ff9f6f98d98e
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-10-18 15:36:56 +0900

    ポリモーフィック関連付け設定

--- app/models/field_machine.rb
+++ app/models/field_machine.rb
@@ -1,2 +1,13 @@
 class FieldMachine < ApplicationRecord
+  belongs_to :field_machineable, polymorphic: true
+
+  before_create -> { self.uuid = SecureRandom.uuid }
+
+  validates :machine_name, presence: true
+  validates :content, presence: true
+  validates :precautions, length: { maximum: 300 }
+
+  def to_param
+    uuid
+  end
 end
