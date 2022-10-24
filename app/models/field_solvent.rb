6b765ff793cb65d506746acdd38fb1a983af1622
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-10-24 15:52:35 +0900

    ポリモーフィック関連付け設定

--- app/models/field_solvent.rb
+++ app/models/field_solvent.rb
@@ -1,2 +1,12 @@
 class FieldSolvent < ApplicationRecord
+  belongs_to :field_solventable, polymorphic: true
+
+  before_create -> { self.uuid = SecureRandom.uuid }
+
+  validates :solvent_name, presence: true
+  validates :content, presence: true
+
+  def to_param
+    uuid
+  end
 end
