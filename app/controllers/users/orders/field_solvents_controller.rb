34ffa8660e5de6e8641531eb6a2f9806361c9950
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-10-25 17:32:09 +0900

    Order側CRUD機能作成

--- app/controllers/users/orders/field_solvents_controller.rb
+++ app/controllers/users/orders/field_solvents_controller.rb
@@ -10,14 +10,36 @@ module Users::Orders
     end
 
     def create
+      ActiveRecord::Base.transaction do
+        params[:solvent_ids].each do |solvent_id|
+          @order.field_solvents.create!(
+            solvent_name: Solvent.find(solvent_id).name,
+            content:      solvent_info(Solvent.find(solvent_id))
+          )
+        end
+        flash[:success] = "#{params[:solvent_ids].count}件追加しました。"
+        redirect_to users_order_field_solvents_url
+      end
+    rescue ActiveRecord::RecordInvalid
+      flash[:danger] = '登録に失敗しました。再度登録してください。'
+      redirect_to users_order_field_solvents_url
     end
 
     def destroy
+      @field_solvent.destroy!
+      flash[:danger] = "#{@field_solvent.solvent_name}を削除しました"
+      redirect_to users_order_field_solvents_url
     end
 
     def edit_solvents; end
 
     def update_solvents
+      field_solvents_params.each do |id, item|
+        field_solvent = FieldSolvent.find(id)
+        field_solvent.update(item)
+      end
+      flash[:success] = '溶剤情報を更新しました'
+      redirect_to users_order_field_solvents_url
     end
 
     private
@@ -32,6 +54,15 @@ module Users::Orders
 
     def set_field_solvents
       @field_solvents = @order.field_solvents
+    end
+
+    def field_solvents_params
+      params.require(:order).permit(
+        field_solvents: %i[
+          solvent_name carried_quantity using_location storing_place using_tool
+          usage_period_start usage_period_end working_process sds ventilation_control
+        ]
+      )[:field_solvents]
     end
   end
 end
--- app/models/field_solvent.rb
+++ app/models/field_solvent.rb
@@ -3,6 +3,9 @@ class FieldSolvent < ApplicationRecord
 
   before_create -> { self.uuid = SecureRandom.uuid }
 
+  enum working_process: { y: 0, n: 1 }, _prefix: true
+  enum sds: { y: 0, n: 1 }, _prefix: true
+
   validates :solvent_name, presence: true
   validates :content, presence: true
 
