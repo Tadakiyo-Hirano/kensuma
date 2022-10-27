fd652edaa95eddca9fb5fcf47e86448045c7c0bd
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-10-27 14:28:01 +0900

    モデルspec作成

--- spec/models/field_solvent_spec.rb
+++ spec/models/field_solvent_spec.rb
@@ -10,6 +10,80 @@ RSpec.describe FieldSolvent, type: :model do
     it 'バリデーションが通ること' do
       expect(subject).to be_valid
     end
+
+    describe '#content' do
+      context '存在しない場合' do
+        before(:each) { subject.content = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#solvent_name' do
+      context '存在しない場合' do
+        before(:each) { subject.solvent_name = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#using_location' do
+      context '100文字の場合' do
+        before(:each) { subject.using_location = 'a' * 100 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.using_location = 'a' * 101 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#storing_place' do
+      context '100文字の場合' do
+        before(:each) { subject.storing_place = 'a' * 100 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.storing_place = 'a' * 101 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#using_tool' do
+      context '100文字の場合' do
+        before(:each) { subject.using_tool = 'a' * 40 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.using_tool = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
   end
 
   describe 'request_order側バリデーションについて' do
@@ -17,6 +91,80 @@ RSpec.describe FieldSolvent, type: :model do
 
     it 'バリデーションが通ること' do
       expect(subject).to be_valid
+    end
+
+    describe '#content' do
+      context '存在しない場合' do
+        before(:each) { subject.content = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#solvent_name' do
+      context '存在しない場合' do
+        before(:each) { subject.solvent_name = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#using_location' do
+      context '100文字の場合' do
+        before(:each) { subject.using_location = 'a' * 100 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.using_location = 'a' * 101 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#storing_place' do
+      context '100文字の場合' do
+        before(:each) { subject.storing_place = 'a' * 100 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.storing_place = 'a' * 101 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#using_tool' do
+      context '100文字の場合' do
+        before(:each) { subject.using_tool = 'a' * 40 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.using_tool = 'a' * 41 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
     end
   end
 end
