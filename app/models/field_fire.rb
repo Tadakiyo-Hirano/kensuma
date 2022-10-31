09622044fe539fb0b0e87647e7c5aea93faee348
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-10-31 13:42:10 +0900

    routes,polymorphic設定

--- app/models/field_fire.rb
+++ app/models/field_fire.rb
@@ -1,2 +1,11 @@
 class FieldFire < ApplicationRecord
+  belongs_to :field_fireable, polymorphic: true
+
+  before_create -> { self.uuid = SecureRandom.uuid }
+
+  validates :use_plase, presence: true, length: { maximum: 100 }
+
+  def to_param
+    uuid
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,12 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
           patch 'update_cars'
         end
       end
+      resources :field_fires, except: %i[new show edit update], module: :orders, param: :uuid do
+        collection do
+          get 'edit_fires'
+          patch 'update_fires'
+        end
+      end
       resources :field_machines, except: %i[new show edit update], module: :orders, param: :uuid do
         collection do
           get 'edit_machines'
@@ -84,6 +90,12 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
         collection do
           get 'edit_cars'
           patch 'update_cars'
+        end
+      end
+      resources :field_fires, except: %i[new show edit update], module: :request_orders, param: :uuid do
+        collection do
+          get 'edit_fires'
+          patch 'update_fires'
         end
       end
       resources :field_machines, except: %i[new show edit update], module: :request_orders, param: :uuid do
