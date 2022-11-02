4726e84b13dc3ad8a87e066de80f02b11cc4a0dc
tsrockt@yahoo.co.jp <chihei.okada@gmail>
2022-11-02 23:52:44 +0900

    会社情報に建設許可証関連のカラムを追加

--- app/controllers/users/businesses_controller.rb
+++ app/controllers/users/businesses_controller.rb
@@ -8,26 +8,35 @@ module Users
       if Rails.env.development?
         @business = Business.new(
           # テスト用デフォルト値 ==========================
-          uuid:                                             '1',
-          name:                                             'test企業',
-          name_kana:                                        'テストキギョウ',
-          branch_name:                                      'test支店',
-          representative_name:                              'test代表',
-          email:                                            'test@email.com',
-          address:                                          'test',
-          post_code:                                        '0123456',
-          phone_number:                                     '01234567898',
-          carrier_up_id:                                    'test',
-          business_type:                                    0,
-          business_health_insurance_status:                 0, # 健康保険(加入状況)
-          business_health_insurance_association:            'テスト健康保険組合', # 健康保険(組合名)
-          business_health_insurance_office_number:          '01234567', # 健康保険(事業所整理記号及び事業所番号)
-          business_welfare_pension_insurance_join_status:   0, # 厚生年金保険(加入状況)
-          business_welfare_pension_insurance_office_number: '01234567890123', # 厚生年金保険(事業所整理記号)
-          business_pension_insurance_join_status:           0, # 年金保険(加入状況)
-          business_employment_insurance_join_status:        0, # 雇用保険(加入状況)
-          business_employment_insurance_number:             '01234567890', # 雇用保険(番号)
-          business_retirement_benefit_mutual_aid_status:    0 # 退職金共済制度(加入状況)
+          uuid:                                                        '1',
+          name:                                                        'test企業',
+          name_kana:                                                   'テストキギョウ',
+          branch_name:                                                 'test支店',
+          representative_name:                                         'test代表',
+          email:                                                       'test@email.com',
+          address:                                                     'test',
+          post_code:                                                   '0123456',
+          phone_number:                                                '01234567898',
+          carrier_up_id:                                               'test',
+          business_type:                                               0,
+          business_health_insurance_status:                            0, # 健康保険(加入状況)
+          business_health_insurance_association:                       'テスト健康保険組合', # 健康保険(組合名)
+          business_health_insurance_office_number:                     '01234567', # 健康保険(事業所整理記号及び事業所番号)
+          business_welfare_pension_insurance_join_status:              0, # 厚生年金保険(加入状況)
+          business_welfare_pension_insurance_office_number:            '01234567890123', # 厚生年金保険(事業所整理記号)
+          business_pension_insurance_join_status:                      0, # 年金保険(加入状況)
+          business_employment_insurance_join_status:                   0, # 雇用保険(加入状況)
+          business_employment_insurance_number:                        '01234567890', # 雇用保険(番号)
+          business_retirement_benefit_mutual_aid_status:               0, # 退職金共済制度(加入状況)
+          construction_license_status:                                 0,                            # 建設許可証(取得状況)
+          construction_license_permission_type_minister_governor:      0,                            # 建設許可証(種別)
+          construction_license_governor_permission_prefecture:         0,                  # 建設許可証(都道府県)
+          construction_license_permission_type_identification_general: 0,                            # 建設許可証(種別)
+          construction_license_number_double_digit:                    29,                           # 建設許可証(番号)
+          construction_license_number_six_digits:                      5000,                         # 建設許可証(番号)
+          construction_license_number:                                 '国土交通大臣(特－29)第5000号', # 建設許可証(建設許可番号)
+          construction_license_updated_at:                             Date.today                    # 建設許可証(更新日)
+
           # =============================================
         )
         @business.business_occupations.build
@@ -87,7 +96,12 @@ module Users
         :business_welfare_pension_insurance_office_number, :business_pension_insurance_join_status,
         :business_employment_insurance_join_status, :business_employment_insurance_number,
         :business_retirement_benefit_mutual_aid_status,
-        occupation_ids: []
+        :construction_license_status, :construction_license_permission_type_minister_governor,
+        :construction_license_governor_permission_prefecture, :construction_license_permission_type_identification_general,
+        :construction_license_number_double_digit, :construction_license_number_six_digits,
+        :construction_license_number, :construction_license_updated_at,
+        occupation_ids: [],
+        industry_ids: []
       )
     end
   end
---
+++ spec/factories/business_industries.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :business_industry do
+    
+  end
+end
