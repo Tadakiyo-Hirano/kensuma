3b202218b749055baeb3a2d1d3d10f8531f07be5
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-11-08 17:33:38 +0900

    modelスペック作成

--- spec/factories/field_fires.rb
+++ spec/factories/field_fires.rb
@@ -1,4 +1,31 @@
 FactoryBot.define do
-  factory :field_fire do
+  factory :order_field_fire, class: 'FieldFire' do
+    field_fireable_type { 'Order' }
+    association :field_fireable, factory: :order
+    uuid { SecureRandom.uuid }
+    sequence(:use_place) { |n| "use_place#{n}" }
+    sequence(:other_usages) { |n| "other_usages#{n}" }
+    usage_period_start { '2022-01-01' }
+    usage_period_end { '2022-01-01' }
+    usage_time_start { '15:00:00' }
+    usage_time_end { '15:00:00' }
+    sequence(:precautions) { |n| "precautions#{n}" }
+    sequence(:fire_origin_responsible) { |n| "fire_origin_responsible#{n}" }
+    sequence(:fire_use_responsible) { |n| "fire_use_responsible#{n}" }
+  end
+
+  factory :request_order_field_fire, class: 'FieldFire' do
+    field_fireable_type { 'RequestOrder' }
+    association :field_fireable, factory: :request_order
+    uuid { SecureRandom.uuid }
+    sequence(:use_place) { |n| "use_place#{n}" }
+    sequence(:other_usages) { |n| "other_usages#{n}" }
+    usage_period_start { '2022-01-01' }
+    usage_period_end { '2022-01-01' }
+    usage_time_start { '15:00:00' }
+    usage_time_end { '15:00:00' }
+    sequence(:precautions) { |n| "precautions#{n}" }
+    sequence(:fire_origin_responsible) { |n| "fire_origin_responsible#{n}" }
+    sequence(:fire_use_responsible) { |n| "fire_use_responsible#{n}" }
   end
 end
--- spec/models/field_fire_spec.rb
+++ spec/models/field_fire_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe FieldFire, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:order_field_fire) { build(:order_field_fire) }
+  let(:request_order_field_fire) { build(:request_order_field_fire) }
+
+  describe 'order側バリデーションについて' do
+    subject { order_field_fire }
+
+    it 'バリデーションが通ること' do
+      expect(subject).to be_valid
+    end
+
+    describe '#use_place' do
+      context '存在しない場合' do
+        before(:each) { subject.use_place = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+
+      context '100文字の場合' do
+        before(:each) { subject.use_place = 'a' * 100 }
+
+        it 'バリデーションが通ること' do
+          expect(subject).to be_valid
+        end
+      end
+
+      context '101文字の場合' do
+        before(:each) { subject.use_place = 'a' * 101 }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#usage_period_start' do
+      context '存在しない場合' do
+        before(:each) { subject.usage_period_start = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#usage_period_end' do
+      context '存在しない場合' do
+        before(:each) { subject.usage_period_end = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#usage_time_start' do
+      context '存在しない場合' do
+        before(:each) { subject.usage_time_start = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+
+    describe '#usage_time_end' do
+      context '存在しない場合' do
+        before(:each) { subject.usage_time_end = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+  end
 end
