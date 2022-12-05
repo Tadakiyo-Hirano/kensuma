dccebebeec62ef2c38be1404c6c9b9537237ed4e
seiichi39 <jhnnw708@gmail.com>
2022-12-05 23:54:34 +0900

    トップ画面のレイアウト修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,4 +49,25 @@ module ApplicationHelper
       request_order.parent_id.nil? ? Order.find(request_order.order_id) : request_order
     end
   end
+
+  #和暦表示
+  def date_select_ja(src_html)
+    dst_html = src_html.gsub(/>\d{4}</) do |m|
+      year = m.match(/>(\d{4})</)[1].to_i
+      year_ja = case year
+        when 2018
+          "平成30/令和元年"
+        else
+          "令和#{year - 2018}"
+      end
+      ">#{year_ja}<"
+    end
+    dst_html.html_safe
+  end
+
+  #会社名の表示
+  def current_business
+    current_user.business || current_user.admin_user.business
+  end
+
 end
