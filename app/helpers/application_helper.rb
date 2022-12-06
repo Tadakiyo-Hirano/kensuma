24f265992ad37eeb6af7fe473b4fd4a15f1abb1a
seiichi39 <jhnnw708@gmail.com>
2022-12-06 09:04:27 +0900

    トップ画面のレイアウト修正2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,24 +50,23 @@ module ApplicationHelper
     end
   end
 
-  #和暦表示
+  # 和暦表示
   def date_select_ja(src_html)
     dst_html = src_html.gsub(/>\d{4}</) do |m|
       year = m.match(/>(\d{4})</)[1].to_i
       year_ja = case year
-        when 2018
-          "平成30/令和元年"
-        else
-          "令和#{year - 2018}"
-      end
+                when 2018
+                  '平成30/令和元年'
+                else
+                  "令和#{year - 2018}"
+                end
       ">#{year_ja}<"
     end
     dst_html.html_safe
   end
 
-  #会社名の表示
+  # 会社名の表示
   def current_business
     current_user.business || current_user.admin_user.business
   end
-
 end
