52682de440606b565aeb07826f35f38a7f1a8c56
ikd-gifu <oki19ki@gmail.com>
2022-12-09 21:26:43 +0900

    add_validation_on_create

--- app/models/field_solvent.rb
+++ app/models/field_solvent.rb
@@ -3,6 +3,10 @@ class FieldSolvent < ApplicationRecord
 
   before_create -> { self.uuid = SecureRandom.uuid }
 
+  before_validation(on: :create) do
+    errors[:base] << '有機溶剤情報は1件までしか登録できません' if field_solventable && field_solventable.field_solvents.count >= 1
+  end
+
   enum working_process: { y: 0, n: 1 }, _prefix: true
   enum sds: { y: 0, n: 1 }, _prefix: true
 
