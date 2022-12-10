ea7e2abf70a73833d4728a15230bbde2cadebc61
ikd-gifu <oki19ki@gmail.com>
2022-12-10 22:11:36 +0900

    fix_doc15th_show.html.reb

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -256,4 +256,36 @@ module DocumentsHelper
       name
     end
   end
+
+  # 有機溶剤情報（使用期間）
+  # date は必ず jisx0301 で変換できる値
+  def wareki(date)
+    _wareki, mon, day = date.jisx0301.split(".")
+    gengou, year = _wareki.partition(/\d+/).take(2)
+  
+    gengou.sub!(/[MTSHR]/,
+                'M' => '明治',
+                'T' => '大正',
+                'S' => '昭和',
+                'H' => '平成',
+                'R' => '令和')
+ 
+    "#{gengou}#{year.to_i}年#{mon.to_i}月#{day.to_i}日"
+  end
+
+  def field_solvent_working_process_y(working_process)
+    working_process == 'y' ? tag.span('有', class: :circle) : '有'
+  end
+
+  def field_solvent_working_process_n(working_process)
+    working_process == 'n' ? tag.span('無', class: :circle) : '無'
+  end
+
+  def field_solvent_sds_y(sds)
+    sds == 'y' ? tag.span('有', class: :circle) : '有'
+  end
+
+  def field_solvent_sds_n(sds)
+    sds == 'n' ? tag.span('無', class: :circle) : '無'
+  end
 end
