300d7acebac2acf54fa1ca458d071eb17fdcda26
Leeo-pro <minerva.aug@gmail.com>
2022-12-20 02:58:39 +0000

    rspecエラー対応10

--- spec/system/machine_spec.rb
+++ spec/system/machine_spec.rb
@@ -16,16 +16,16 @@ RSpec.xdescribe 'Machines', type: :system do
       fill_in 'user[password]', with: user.password
       click_button 'ログイン'
     end
-    
+
     it 'ログイン後持込機械情報一覧へ画面遷移できること' do
       visit users_machines_path
       expect(page).to have_content '持込機械情報一覧'
     end
-    
+
     context '持込機械情報登録' do
       it '新規登録した後に詳細画面へ遷移できること' do
         visit new_users_machine_path
-        
+
         select '電動カンナ', from: 'machine[name]'
         fill_in 'machine[standards_performance]', with: machine.standards_performance
         fill_in 'machine[control_number]', with: machine.control_number
@@ -34,50 +34,50 @@ RSpec.xdescribe 'Machines', type: :system do
         fill_in 'machine[inspection_date]', with: machine.inspection_date
         fill_in 'machine[extra_inspection_item1]', with: machine.extra_inspection_item1
         fill_in 'machine[extra_inspection_item2]', with: machine.extra_inspection_item2
-        
+
         click_button '登録'
         expect(page).to have_content '持込機械情報を登録しました'
       end
     end
-    
-    context '持込機械情報編集-重複削除（新規登録後）' do
-      it '追加点検事項の重複が削除されていること' do
-        visit edit_users_machine_path(machine)
-        
-        expect(Machine.last.extra_inspection_item2).not_to eq ''
-      end
-    end
-    
-    context '持込機械情報編集' do
-      it '持込機械情報が編集できること' do
-        visit edit_users_machine_path(machine)
-        
-        select '電動ドリル', from: 'machine[name]'
-        fill_in 'machine[extra_inspection_item2]', with: 'test'
-        
-        click_button '更新'
-        expect(page).to have_content '更新しました'
-      end
-    end
-    
-    context '持込機械情報編集-重複削除（更新後）' do
+
+    context '持込機械情報編集の重複確認（新規登録）' do
       it '追加点検事項の重複が削除されていること' do
         visit edit_users_machine_path(machine)
 
         expect(Machine.last.extra_inspection_item2).not_to eq ''
       end
     end
-    
+
+    context '持込機械情報編集' do
+      it '持込機械情報が編集できること' do
+        visit edit_users_machine_path(machine)
+
+        select '電動ドリル', from: 'machine[name]'
+        fill_in 'machine[extra_inspection_item2]', with: 'test'
+
+        click_button '更新'
+        expect(page).to have_content '更新しました'
+      end
+    end
+
+    context '持込機械情報編集の重複確認（更新）' do
+      it '追加点検事項の重複が削除されていること' do
+        visit edit_users_machine_path(machine)
+
+        expect(Machine.last.extra_inspection_item2).not_to eq ''
+      end
+    end
+
     context '持込機械情報削除' do
       it '持込機械情報を削除できること', js: true do
         visit users_machine_path(machine)
         click_on '削除'
-        
+
         expect {
         expect(page.accept_confirm).to eq "#{machine.name}の持込機械情報を削除します。本当によろしいですか？"
         expect(page).to have_content "#{machine.name}を削除しました"
         }.to change(Machine, :count).by(-1)
-        
+
         visit users_machines_path
         expect(page).to have_content '持込機械情報一覧'
       end
