6c3ae013c23c04e50323df7dbf7b4b2d185bb598
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-12-26 17:32:52 +0900

    元請け表示分け

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register User do
   # Uncomment all parameters which should be permitted for assignment
   #
 
-  permit_params :email, :password, :password_confirmation, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :name, :role
+  permit_params :email, :password, :password_confirmation, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :name, :role, :is_prime_contractor
 
   controller do
     def scoped_collection
@@ -16,7 +16,7 @@ ActiveAdmin.register User do
   index do
     column :name
     column :email
-    column :is_prime_contractor
+    bool_column :is_prime_contractor
     column :role, &:role_i18n
 
     # 閲覧編集削除などのリンク表示
@@ -29,13 +29,21 @@ ActiveAdmin.register User do
     attributes_table do
       row :name
       row :email
+      bool_row :is_prime_contractor
+      row :role, &:role_i18n
     end
   end
 
   form do |f|
+    @form = f.object
+    if @form.new_record?
+      @form[:is_prime_contractor] = true
+    end
+
     f.inputs do
       f.input :name
       f.input :email
+      f.input :is_prime_contractor, hint: '下請けに変更する場合はチェックを外してください'
       f.input :password
       f.input :password_confirmation
       # パスワードの一時表示一旦コメントアウト
