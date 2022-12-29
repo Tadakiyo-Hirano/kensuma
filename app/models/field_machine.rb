84677fec8f9fdf2bcc1e193b6ffc5ceae6e96c00
ikd-gifu <oki19ki@gmail.com>
2022-12-29 18:33:09 +0900

    fix_field_machine_validation_on_create

--- app/models/field_machine.rb
+++ app/models/field_machine.rb
@@ -3,6 +3,10 @@ class FieldMachine < ApplicationRecord
 
   before_create -> { self.uuid = SecureRandom.uuid }
 
+  before_validation(on: :create) do
+    errors[:base] << '現場機械情報は10件までしか登録できません' if field_machineable && field_machineable.field_machines.count >= 10
+  end
+
   validates :machine_name, presence: true
   validates :content, presence: true
 
