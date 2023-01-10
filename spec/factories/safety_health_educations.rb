63b32a0df92a47b6d402e9c5ff6bacdd11fe8d05
Tadakiyo-Hirano <d85nus03@gmail.com>
2023-01-10 13:54:52 +0900

    Rspec作成

--- spec/factories/safety_health_educations.rb
+++ spec/factories/safety_health_educations.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :safety_health_education do
-    
+    name { 'TEST_NAME' }
+    description { 'TEST_DESCRIPTION' }
   end
 end
--- spec/models/safety_health_education_spec.rb
+++ spec/models/safety_health_education_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe SafetyHealthEducation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:safety_health_education) { create(:safety_health_education) }
+
+  describe 'バリデーションについて' do
+    subject { safety_health_education }
+
+    it 'バリデーションが通ること' do
+      expect(subject).to be_valid
+    end
+
+    describe '#name' do
+      context '存在しない場合' do
+        before(:each) { subject.name = nil }
+
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+      end
+    end
+  end
 end
