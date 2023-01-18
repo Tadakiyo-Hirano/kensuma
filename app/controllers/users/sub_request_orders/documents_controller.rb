61d7d071bf34f452f9c10b611db1c71ef0d206f1
Tadakiyo-Hirano <d85nus03@gmail.com>
2023-01-18 17:23:22 +0900

    元請け編集可能画面の切り分け

--- app/controllers/users/sub_request_orders/documents_controller.rb
+++ app/controllers/users/sub_request_orders/documents_controller.rb
@@ -1,5 +1,6 @@
 module Users::SubRequestOrders
   class DocumentsController < Users::Base
+    # before_action :set_document
     layout 'documents'
 
     def index
@@ -34,6 +35,30 @@ module Users::SubRequestOrders
       render 'users/documents/show'
     end
 
+    def edit
+      sub_request_order = validate_request_order!
+      @documents = sub_request_order.documents
+      @document = @documents.find_by!(uuid: params[:uuid])
+
+      render 'users/documents/edit'
+    end
+
+    def update
+      sub_request_order = validate_request_order!
+      @documents = sub_request_order.documents
+      @document = @documents.find_by!(uuid: params[:uuid])
+      
+      case @document.document_type
+      when 'doc_16th'
+        if @document.update(document_params(@document))
+          redirect_to users_request_order_sub_request_order_document_url, success: '保存に成功しました'
+        else
+          flash[:danger] = '更新に失敗しました'
+          render :edit
+        end
+      end
+    end
+
     private
 
     def validate_request_order!
@@ -44,6 +69,25 @@ module Users::SubRequestOrders
       end
 
       sub_request_order
+    end
+
+    def document_params(document)
+      case document.document_type
+      when 'doc_16th'
+        params.require(:document).permit(content: 
+          [
+            :fire_permit_number,
+            :fire_permit_date,
+            :fire_prevention_manager,
+            :manager,
+            :permit_criteria
+          ]
+        )
+      end
+    end
+
+    def set_document
+      # @document = RequestOrder.find_by(uuid: params[:sub_request_order_uuid]).documents.find_by(uuid: params[:uuid])
     end
   end
 end
