51d9d44014e4222d4a11985e1e630a8564f89a24
Tadakiyo-Hirano <d85nus03@gmail.com>
2023-01-19 17:26:42 +0900

    documentモデル approval_contentカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_01_063423) do
+ActiveRecord::Schema.define(version: 2023_01_19_081919) do
 
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "namespace"
@@ -169,6 +169,7 @@ ActiveRecord::Schema.define(version: 2023_01_01_063423) do
     t.json "content"
     t.bigint "business_id", null: false
     t.bigint "request_order_id", null: false
+    t.json "approval_content"
     t.index ["business_id"], name: "index_documents_on_business_id"
     t.index ["request_order_id"], name: "index_documents_on_request_order_id"
   end
