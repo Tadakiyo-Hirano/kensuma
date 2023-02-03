54919136e0f4b979fe86eead020b81e196108370
Tadakiyo-Hirano <d85nus03@gmail.com>
2023-02-03 15:04:03 +0900

    checkbox表示修正

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -498,6 +498,10 @@ module DocumentsHelper
     end
   end
 
+  def checked_none_box(checked_status)
+    '✓' if checked_status == '1'
+  end
+
   # リスクの見積り
   def risk_estimation_level(risk_possibility, risk_seriousness)
     possibility_point, _possibility_comment = risk_possibility(risk_possibility)
