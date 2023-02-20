1a6ab74c32b18ccb3e7da9265694171ead8f7531
shinya <shinya.works.w@gmail.com>
2023-02-21 00:28:39 +0900

    puroductionのseedファイルのマスタ情報以外をコメントアウト

--- db/fixtures/production/17_news.rb
+++ db/fixtures/production/17_news.rb
@@ -1,11 +1,11 @@
-20.times do |n|
-  News.seed(
-    {
-      id: n+1,
-      title: "お知らせ-#{n+1}",
-      content: "改行テスト-#{n+1}\r\n改行\r再度改行\n最終行".gsub(/\R/, "\n"),
-      delivered_at: DateTime.now - (n+1),
-      status: 'published'
-    }
-  )
-end
+# 20.times do |n|
+#   News.seed(
+#     {
+#       id: n+1,
+#       title: "お知らせ-#{n+1}",
+#       content: "改行テスト-#{n+1}\r\n改行\r再度改行\n最終行".gsub(/\R/, "\n"),
+#       delivered_at: DateTime.now - (n+1),
+#       status: 'published'
+#     }
+#   )
+# end
