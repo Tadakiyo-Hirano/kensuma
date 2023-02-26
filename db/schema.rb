b4cafd126fe0724c6e5a6b5de80c7db492da5e38
shinya <shinya.works.w@gmail.com>
2023-02-26 22:23:48 +0900

    workerテーブルのデータ型変更、および不要カラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -772,16 +772,15 @@ ActiveRecord::Schema.define(version: 2023_01_31_030912) do
     t.string "relationship", null: false
     t.string "email"
     t.integer "sex", default: 0, null: false
-    t.integer "status_of_residence", default: 0, null: false
+    t.integer "status_of_residence"
     t.date "maturity_date"
-    t.integer "confirmed_check", default: 0, null: false
+    t.integer "confirmed_check"
     t.date "confirmed_check_date"
-    t.json "responsible_director"
-    t.string "responsible_name"
-    t.integer "responsible_contact_address"
-    t.json "passport"
-    t.json "residence_card"
-    t.json "employment_conditions"
+    t.string "passport_front"
+    t.string "passport_back"
+    t.string "residence_card_front"
+    t.string "residence_card_back"
+    t.string "employment_condition"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["business_id"], name: "index_workers_on_business_id"
