51ac516c49b32435518f399ad7678e47cd0c97c4
ikd-gifu <oki19ki@gmail.com>
2023-02-26 14:06:35 +0900

    add_systemspec_except_select2_input_form

--- spec/factories/field_workers.rb
+++ spec/factories/field_workers.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     field_workerable_type { 'Order' }
     association :field_workerable, factory: :order
     uuid { SecureRandom.uuid }
+    education_date { '2023-01-28' }
     sequence(:admission_worker_name) { |n| "admission_worker_name#{n}" }
     sequence(:content) { |n| { "id": n + 1 } }
   end
@@ -11,6 +12,7 @@ FactoryBot.define do
     field_workerable_type { 'RequestOrder' }
     association :field_workerable, factory: :request_order
     uuid { SecureRandom.uuid }
+    education_date { '2023-01-28' }
     sequence(:admission_worker_name) { |n| "admission_worker_name#{n}" }
     sequence(:content) { |n| { "id": n + 1 } }
   end
