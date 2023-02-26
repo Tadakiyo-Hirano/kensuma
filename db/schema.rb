da6d3be601dad63f08217ef7eca599a2e1bd6784
ikd-gifu <oki19ki@gmail.com>
2023-02-26 21:35:39 +0900

    fix_doc_15th_show/edit

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 2023_01_31_030912) do
 
   create_table "businesses", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "uuid", null: false
-    t.integer "business_type", null: false
     t.string "name", null: false
     t.string "name_kana", null: false
     t.string "branch_name", null: false
@@ -92,17 +91,9 @@ ActiveRecord::Schema.define(version: 2023_01_31_030912) do
     t.string "address", null: false
     t.string "post_code", null: false
     t.string "phone_number", null: false
-    t.string "fax_number"
-    t.string "career_up_id"
-    t.json "career_up_card_copy"
+    t.string "carrier_up_id"
     t.json "stamp_images"
-    t.json "occupation_ids"
-    t.json "industry_ids"
-    t.integer "specific_skilled_foreigners_exist"
-    t.integer "foreign_construction_workers_exist"
-    t.integer "foreign_technical_intern_trainees_exist"
-    t.string "employment_manager_name"
-    t.string "employment_manager_post"
+    t.integer "business_type", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -648,8 +639,6 @@ ActiveRecord::Schema.define(version: 2023_01_31_030912) do
     t.string "name"
     t.integer "age"
     t.integer "gender"
-    t.json "invited_user_ids"
-    t.json "invitation_sent_user_ids"
     t.integer "role", default: 1
     t.bigint "admin_user_id"
     t.string "invitation_token"
@@ -762,28 +751,13 @@ ActiveRecord::Schema.define(version: 2023_01_31_030912) do
     t.date "hiring_on", null: false
     t.integer "experience_term_before_hiring", null: false
     t.integer "blank_term", null: false
-    t.string "career_up_id"
+    t.string "carrier_up_id"
     t.json "images"
     t.bigint "business_id", null: false
-    t.string "uuid", null: false
-    t.string "job_title", null: false
-    t.integer "employment_contract", default: 0, null: false
-    t.string "family_name", null: false
-    t.string "relationship", null: false
-    t.string "email"
-    t.integer "sex", default: 0, null: false
-    t.integer "status_of_residence", default: 0, null: false
-    t.date "maturity_date"
-    t.integer "confirmed_check", default: 0, null: false
-    t.date "confirmed_check_date"
-    t.json "responsible_director"
-    t.string "responsible_name"
-    t.integer "responsible_contact_address"
-    t.json "passport"
-    t.json "residence_card"
-    t.json "employment_conditions"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uuid", null: false
+    t.string "job_title", null: false
     t.index ["business_id"], name: "index_workers_on_business_id"
   end
 
