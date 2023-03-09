4ef7b4a03f3d0c9cb7dfe2e8fa2a222af59dc16f
shinya <shinya.works.w@gmail.com>
2023-03-09 20:43:57 +0900

    作業員情報のフォームの非表示にモデル、コントローラーの変更

--- app/models/worker_insurance.rb
+++ app/models/worker_insurance.rb
@@ -31,8 +31,20 @@ class WorkerInsurance < ApplicationRecord
   enum has_labor_insurance: { join: 0, not_join: 1 }, _prefix: true       # 労働保険特別加入の有無
 
   validates :health_insurance_type, presence: true
+  validates :health_insurance_name, presence: true, if: :insurance_name_valid?
   validates :pension_insurance_type, presence: true
   validates :employment_insurance_type, presence: true
-  validates :employment_insurance_number, { length: { is: 4 } }
+  validates :employment_insurance_number, length: { is: 4 }, if: :employment_insurance_number_valid?
   validates :severance_pay_mutual_aid_type, presence: true
+
+  private
+    # 健康保険が健康保険組合もしくは建設国保であればtrue
+    def insurance_name_valid?
+      health_insurance_type == "health_insurance_association" || health_insurance_type == "construction_national_health_insurance"
+    end
+
+    # 雇用保険が被保険者であればtrue
+    def employment_insurance_number_valid?
+      employment_insurance_type == "insured"
+    end
 end
