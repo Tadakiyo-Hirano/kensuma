a9e17a839288b4879d075b56186c1fa4f4973c52
shinya-okuno <shinya.works.w@gmail.com>
2023-03-25 22:46:23 +0900

    安全衛生教育モデルのモデルファイル修正

--- app/models/safety_health_education.rb
+++ app/models/safety_health_education.rb
@@ -1,3 +1,6 @@
 class SafetyHealthEducation < ApplicationRecord
+  has_many :safety_health_educations, dependent: :destroy
+  has_many :workers, through: :safety_health_educations
+  
   validates :name, presence: true
 end
