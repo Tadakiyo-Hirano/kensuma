20295bcc318a90411bd7945c70759833a5d969fd
Tadakiyo-Hirano <d85nus03@gmaul.com>
2023-04-12 21:13:58 +0900

    書類名変更

--- app/models/document.rb
+++ app/models/document.rb
@@ -11,42 +11,42 @@ class Document < ApplicationRecord
   before_create -> { self.uuid = SecureRandom.uuid }
 
   # 自身の書類一覧取得(自身が元請の場合、一次の場合、二次の場合、三次以降の場合)
-  scope :genecon_documents_type, -> { where(document_type: [1, 2, 3, 4, 7, 18, 19, 20, 22, 23]) }
-  scope :first_subcon_documents_type, -> { where(document_type: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]) }
-  scope :second_subcon_documents_type, -> { where(document_type: [3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 21, 22, 24]) }
-  scope :third_or_later_subcon_documents_type, -> { where(document_type: [3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 21, 22, 24]) }
+  scope :genecon_documents_type, -> { where(document_type: [1, 2, 3, 4, 7, 18, 19, 20, 22, 23]).order(Arel.sql("FIELD(document_type, 1, 2, 3, 4, 7, 19, 20, 23, 18, 22)")) }
+  scope :first_subcon_documents_type, -> { where(document_type: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]).order(Arel.sql("FIELD(document_type, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 23, 18, 22)")) }
+  scope :second_subcon_documents_type, -> { where(document_type: [3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 21, 22, 24]).order(Arel.sql("FIELD(document_type, 3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 21, 24, 18, 22)")) }
+  scope :third_or_later_subcon_documents_type, -> { where(document_type: [3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 21, 22, 24]).order(Arel.sql("FIELD(document_type, 3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 21, 24, 18, 22)")) }
   # 元請け配下の一次下請け書類一覧取得
-  scope :current_lower_first_documents_type, -> { where(document_type: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,  21, 22, 23, 24]) }
+  scope :current_lower_first_documents_type, -> { where(document_type: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]).order(Arel.sql("FIELD(document_type, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 23, 24, 18, 22)")) }
   # 一次下請け配下の二次下請け書類一覧取得
-  scope :first_lower_second_documents_type, -> { where(document_type: [3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 21, 22, 24]) }
+  scope :first_lower_second_documents_type, -> { where(document_type: [3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 21, 22, 24]).order(Arel.sql("FIELD(document_type, 3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 21, 24, 18, 22)")) }
   # 二次下請け以降の配下の書類一覧取得(二次→三次、三次→四次)
-  scope :lower_other_documents_type, -> { where(document_type: [3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 21, 22, 24]) }
+  scope :lower_other_documents_type, -> { where(document_type: [3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 21, 22, 24]).order(Arel.sql("FIELD(document_type, 3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 21, 24, 18, 22)")) }
 
   enum document_type: {
     cover_document:             1,  # 表紙
     table_of_contents_document: 2,  # 目次
     doc_3rd:                    3,  # 施工体制台帳作成建設工事の通知
-    doc_4th:                    4,  # 全建統一様式第３号(施工体制台帳)
-    doc_5th:                    5,  # 全建統一様式第３号(施工体制台帳)
+    doc_4th:                    4,  # 施工体制台帳【全建統一様式第３号】
+    doc_5th:                    5,  # 再下請負通知書(変更届)【全建統一様式第１号－甲】
     doc_6th:                    6,  # 安全衛生管理に関する契約書(一次会社用)
     doc_7th:                    7,  # 安全衛生管理に関する契約書(再下請会社用)
     doc_8th:                    8,  # 作業員名簿
-    doc_9th:                    9,  # 全建統一様式第１号-甲-別紙(外国人建設就労者建設現場入場届出書)
+    doc_9th:                    9,  # 外国人建設就労者建設現場入場届出書【全建統一様式第１号-甲-別紙】
     doc_10th:                   10, # 高齢者就労報告書
     doc_11th:                   11, # 年少者就労報告書
     doc_12th:                   12, # 工事用・通勤用車両届
-    doc_13th:                   13, # 全建統一様式第９号([移動式クレーン／車両系建設機械等]使用届)
-    doc_14th:                   14, # 参考様式第６号(持込機械等(電動工具電気溶接機等)使用届
-    doc_15th:                   15, # 全建統一様式第１１号(有機溶剤・特定化学物質等持込使用届)
-    doc_16th:                   16, # 参考様式第９号(火気使用届)
-    doc_17th:                   17, # 全建統一様式第１号－乙(下請負業者編成表)
-    doc_18th:                   18, # 全建統一様式第４号(工事作業所災害防止協議会兼施工体系図)
-    doc_19th:                   19, # 全建統一様式第６号(工事安全衛生計画書)
-    doc_20th:                   20, # 参考様式第３号(年間安全衛生計画書)
-    doc_21st:                   21, # 全建統一様式第７号(新規入場時等教育実施報告書)
-    doc_22nd:                   22, # 参考様式第５号(作業間連絡調整書)
-    doc_23rd:                   23, # 全建統一様式第８号(安全ミーティング報告書)
-    doc_24th:                   24  # 参考資料第４号(新規入場者調査票)
+    doc_13th:                   13, # [移動式クレーン／車両系建設機械等]使用届【全建統一様式第９号】
+    doc_14th:                   14, # 持込機械等(電動工具電気溶接機等)使用届【参考様式第６号】
+    doc_15th:                   15, # 有機溶剤・特定化学物質等持込使用届【全建統一様式第１１号】
+    doc_16th:                   16, # 火気使用届【参考様式第９号】
+    doc_17th:                   17, # 下請負業者編成表【全建統一様式第１号－乙】
+    doc_18th:                   18, # 工事作業所災害防止協議会兼施工体系図【全建統一様式第４号】
+    doc_19th:                   19, # 工事安全衛生計画書【全建統一様式第６号】
+    doc_20th:                   20, # 年間安全衛生計画書【参考様式第３号】
+    doc_21st:                   21, # 新規入場時等教育実施報告書【全建統一様式第７号】
+    doc_22nd:                   22, # 作業間連絡調整書【参考様式第５号】
+    doc_23rd:                   23, # 安全ミーティング報告書【全建統一様式第８号】
+    doc_24th:                   24  # 新規入場者調査票【参考資料第４号】
   }
 
   # 制限に関するグローバル変数(doc_20th)
