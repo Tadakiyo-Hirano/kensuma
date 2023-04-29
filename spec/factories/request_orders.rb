c7590fb87443e6affb988cd203fd5c9e8ba46928
Tadakiyo-Hirano <d85nus03@gmaul.com>
2023-04-29 20:59:53 +0900

    rspec修正

--- spec/factories/request_orders.rb
+++ spec/factories/request_orders.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :request_order do
     uuid { SecureRandom.uuid }
+    status { 0 }
     sequence(:occupation) { |n| "occupation#{n}" }
     sequence(:construction_name) { |n| "construction_name#{n}" }
     sequence(:construction_details) { |n| "construction_details#{n}" }
