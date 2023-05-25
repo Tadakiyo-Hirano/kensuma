1baa03ab7aba58572c968893a16d82f8d5c946a7
Tadakiyo-Hirano <d85nus03@gmaul.com>
2023-05-26 01:03:35 +0900

    施工場所住所の必須を外す

--- spec/factories/businesses.rb
+++ spec/factories/businesses.rb
@@ -13,7 +13,7 @@ FactoryBot.define do
     business_type { 0 }
     business_health_insurance_status { 0 } # 健康保険(加入状況)
     business_health_insurance_association { 'TEST健康保険組合' } # 健康保険(組合名)
-    business_health_insurance_office_number { '01234567' } # 健康保険(事業所整理記号及び事業所番号)
+    business_health_insurance_office_number { '0123' } # 健康保険(事業所整理記号及び事業所番号)
     business_welfare_pension_insurance_join_status { 0 } # 厚生年金保険(加入状況)
     business_welfare_pension_insurance_office_number { '01234567890123' } # 厚生年金保険(事業所整理記号)
     business_employment_insurance_join_status { 0 } # 雇用保険(加入状況)
