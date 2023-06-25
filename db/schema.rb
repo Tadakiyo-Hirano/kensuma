288b37a2d08ffb646e0fb55938cf348feb108d08
akito sato <eight2019@outlook.jp>
2023-06-26 00:45:39 +0900

    schema変更後

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_25_151249) do
+ActiveRecord::Schema.define(version: 2023_06_10_135431) do
 
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "namespace"
