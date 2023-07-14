d6af60bbf5576b75b523001dc389e6220ca46f51
akito sato <eight2019@outlook.jp>
2023-07-15 06:04:25 +0900

    203番の不具合修正

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1605,8 +1605,15 @@ module DocumentsHelper
   #doc_9
   # 自身の一つ上階層の会社情報&現場情報取得
   def get_myself_and_myparent_site
-    request_order = RequestOrder.find_by(uuid: params[:request_order_uuid])
-    @parent_request_orber = RequestOrder.find(request_order.parent_id)
+    request_order_uuid = params[:request_order_uuid]
+    return if request_order_uuid.nil?
+  
+    request_order = RequestOrder.find_by(uuid: request_order_uuid)
+    return if request_order.nil?
+  
+    @parent_request_orber = RequestOrder.find_by(id: request_order.parent_id)
+    return if @parent_request_orber.nil?
+  
     @parent_business = @parent_request_orber.business
   end
   # rubocop:enable all
