e15122ab4d532a7ccfaa165c68684ac92c5d5010
Tadakiyo-Hirano <d85nus03@gmaul.com>
2023-11-18 21:16:41 +0900

    fix worker default data

--- app/helpers/workers_helper.rb
+++ app/helpers/workers_helper.rb
@@ -70,14 +70,11 @@ module WorkersHelper
     )
     @worker.build_worker_insurance(
       # テスト用デフォルト値 ==========================
-      health_insurance_type:         :health_insurance_association,
-      health_insurance_name:         'サンプル健康保険',
+      health_insurance_type:         :not_health_insurance,
       pension_insurance_type:        :welfare,
-      employment_insurance_type:     :insured,
-      employment_insurance_number:   '1234',
-      severance_pay_mutual_aid_type: :kentaikyo,
-      severance_pay_mutual_aid_name: 'テスト共済制度',
-      has_labor_insurance:           :join
+      employment_insurance_type:     :exemption,
+      severance_pay_mutual_aid_type: :none,
+      # has_labor_insurance:           :not_join
       # ============================================
     )
   end
@@ -98,19 +95,19 @@ module WorkersHelper
     @worker.worker_special_educations.build
     @worker.build_worker_medical(
       # 本番環境用デフォルト値 ==========================
-      is_med_exam:         :y,
-      is_special_med_exam: :y,
+      is_med_exam:         :n,
+      is_special_med_exam: :n,
       health_condition:    :good
       # ============================================
     )
     @worker.worker_safety_health_educations.build
     @worker.build_worker_insurance(
       # 本番環境用デフォルト値 ==========================
-      health_insurance_type:         :health_insurance_association,
+      health_insurance_type:         :not_health_insurance,
       pension_insurance_type:        :welfare,
-      employment_insurance_type:     :insured,
-      severance_pay_mutual_aid_type: :kentaikyo,
-      has_labor_insurance:           :join
+      employment_insurance_type:     :exemption,
+      severance_pay_mutual_aid_type: :none,
+      # has_labor_insurance:           :not_join
       # ============================================
     )
   end
